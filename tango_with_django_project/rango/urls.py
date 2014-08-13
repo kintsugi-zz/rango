178a49a1c60e1bbebd58b0cd8b5b7865c9a28014
kintsugi <mirumvis@gmail.com>
2014-08-13 17:11:48 -0500

    ch12 complete - search bing api functionality

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -4,6 +4,7 @@ from rango import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
+    url(r'^search/$', views.search, name='search'),
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', views.user_logout, name='logout'),
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,6 +7,7 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
+from rango.bing_search import run_query
 
 def encode_url(str):
     return str.replace(' ', '_')
@@ -16,75 +17,8 @@ def decode_url(str):
 
 @login_required
 def restricted(request):
-    #return HttpResponse("Since you're logged in, you can see this text!")
     context = RequestContext(request)
     return render_to_response('rango/restricted.html', {}, context)
-
-"""
-def index(request):
-    # Obtain the context from the HTTP request.
-    context = RequestContext(request)
-
-    # Query for categories - add the list to our context dictionary.
-    category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories': category_list,
-    				'pages': page_list,
-    				}
-
-    # The following two lines are new.
-    # We loop through each category returned, and create a URL attribute.
-    # This attribute stores an encoded URL (e.g. spaces replaced with underscores).
-    for category in category_list:
-        category.url = encode_url(category.name)
-
-    # Render the response and return to the client.
-    #return render_to_response('rango/index.html', context_dict, context)
-    response = render_to_response('rango/index.html', context_dict, context)
-    visits = int(request.COOKIES.get('visits', '0'))
-    if 'last_visit' in request.COOKIES:
-        last_visit = request.COOKIES['last_visit']
-        last_visit_time = datetime.strptime(last_visit[:-7], '%Y-%m-%d %H:%M:%S')
-        if (datetime.now() - last_visit_time).days > 0:
-            response.set_cookie('visits', visits+1)
-            response.set_cookie('last_visit', datetime.now())
-    else:
-        response.set_cookie('last_visit', datetime.now())
-    return response
-"""
-def index(request):
-    context = RequestContext(request)
-
-    category_list = Category.objects.all()
-    context_dict = {'categories': category_list}
-
-    for category in category_list:
-        category.url = encode_url(category.name)
-
-    page_list = Page.objects.order_by('-views')[:5]
-    context_dict['pages'] = page_list
-
-    #### NEW CODE ####
-    # Obtain our Response object early so we can add cookie information.
-    response = render_to_response('rango/index.html', context_dict, context)
-    visits = int(request.COOKIES.get('visits', '0'))
-    # Does the cookie last_visit exist?
-    if 'last_visit' in request.COOKIES:
-        # Yes it does! Get the cookie's value.
-        last_visit = request.COOKIES['last_visit']
-        # Cast the value to a Python date/time object.
-        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
-        # If it's been more than a day since the last visit...
-        if (datetime.now() - last_visit_time).days > 0:
-            # ...reassign the value of the cookie to +1 of what it was before...
-            response.set_cookie('visits', visits+1)
-            # ...and update the last visit cookie, too.
-            response.set_cookie('last_visit', datetime.now())
-    else:
-        # Cookie last_visit doesn't exist, so create it to the current date/time.
-        response.set_cookie('last_visit', datetime.now())
-    # Return response back to the user, updating any cookies 
-    return response
 
 def index(request):
     context = RequestContext(request)
@@ -282,3 +216,15 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect('/rango/')
+
+def search(request):
+    context = RequestContext(request)
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            result_list = run_query(query)
+
+    return render_to_response('rango/search.html', {'result_list': result_list}, context)
